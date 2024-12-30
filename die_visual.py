88cafe87bddbbce15459b7e81a045fe78e431e24
rp.parthi <rp.paarthi@gmail.com>
2024-12-31 02:29:36 +0530

    create plotly bar chart

---
+++ die_visual.py
@@ -0,0 +1,23 @@
+from die import Die
+import plotly.express as px
+import pandas
+
+# Create a D6.
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyse the results
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
